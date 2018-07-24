dafbd009c1cde02670a36bf285178f50252a404e
dongjun77 <edj0707@naver.com>
2018-07-24 18:57:55 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+
+    if (req.url === '/'){//요청 객체
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Hello World\n');
+    } else if (req.url === '/users') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('User list')
+    } else {
+        res.statusCode = 404;
+        res.end('Not Found')
+    }
+    
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
